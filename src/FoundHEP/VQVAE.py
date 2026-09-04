20fe0a786f6d9f8dfd9ba2d4de82e3a0a80fd878
Marco Lorusso <marco.lorusso11@unibo.it>
2026-09-04 17:49:45 +0200

    Used the non-causal transformer also as decoder

--- src/FoundHEP/VQVAE.py
+++ src/FoundHEP/VQVAE.py
@@ -58,30 +58,40 @@ class VQVAE(keras.Model):
     
     def __init__(self, original_dim, hidden_dim= 64, latent_dim = 32, name = "vae", **kwargs):
         super().__init__(name = name, **kwargs)
-        data = np.load("/home/marco/FoundHEP/dataset.npy")
-        
-        print(data.shape)
-        print(data[0])
+        #data = np.load("/home/marco/FoundHEP/dataset.npy")
+        #
+        #print(data.shape)
+        #print(data[0])
         self.original_dim = original_dim
 #        self.encoder = Encoder()
+        self.encoder_input = keras.layers.Dense(hidden_dim)
         self.quantizer = VectorQuantizerEMA(num_embeddings = 2048, embedding_dim = latent_dim, commitment_cost = 0.25, decay = 0.9)
 #        self.decoder = Decoder(original_dim)
 #        self.sampling = Sampling()
-        self.transencoder = TransEncoder()
-        self.transdecoder = TransDecoder()
+        self.transencoder = TransEncoder(out_dim = hidden_dim, num_heads = 16)
+        self.to_latent = keras.layers.Dense(latent_dim)
+        self.decoder_input = keras.layers.Dense(hidden_dim)
+        self.transdecoder = TransEncoder(out_dim = hidden_dim, num_heads = 16)
+        self.output_projection = keras.layers.Dense(original_dim)
 
-    def call(self, inputs):
+    def call(self, inputs, attention_mask = None, training = None):
 
-        encoded_input = self.transencoder(inputs)
-        embedded_input = self.quantizer(encoded_input)
-        decoded_latent = self.transdecoder(inputs, embedded_input)
+        x = self.encoder_input(inputs)
+
+        encoded_input = self.transencoder(x, attention_mask = attention_mask, training=training)
+        z_e = self.to_latent(encoded_input)
+        embedded_input = self.quantizer(z_e, training = training)["quantize"]
+
+        embedded_input = self.decoder_input(embedded_input)
+
+        decoded_latent = self.transdecoder(embedded_input, attention_mask = attention_mask, training = training)
         #z_mean, z_log_var= self.encoder(x)
         #z = self.sampling((z_mean, z_log_var))
         #reco = self.decoder(z)
 
         #kl_loss = -0.5 * keras.ops.mean(z_log_var - keras.ops.square(z_mean) - keras.ops.exp(z_log_var) + 1)
         #self.add_loss(kl_loss)
-        return decoded_latent
+        return self.output_projection(decoded_latent)
     
 
 
